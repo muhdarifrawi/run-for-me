e8f60770427506d8db78da4c6c64dd0165f4e262
Ubuntu <ubuntu@ip-172-31-92-12.ec2.internal>
2019-11-11 08:52:56 +0000

    created landing/main page.

--- RunForMe/urls.py
+++ RunForMe/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from main.views import show_main
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", show_main)
 ]
--- main/views.py
+++ main/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def show_main(request):
-    return HttpResponse("Hello world")
+    return render(request, "index.template.html")
