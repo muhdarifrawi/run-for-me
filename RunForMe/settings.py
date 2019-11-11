c2e6eb6897f4f9cc53af22629eb6187972d61e32
Ubuntu <ubuntu@ip-172-31-92-12.ec2.internal>
2019-11-11 07:15:23 +0000

    django installed successfully.

--- RunForMe/settings.py
+++ RunForMe/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'd7+i)ne_ynmmxiu6x^u@+-s2%cv7-lnhrb1_%s6$%sbfhi##93'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "main"
 ]
 
 MIDDLEWARE = [
--- main/views.py
+++ main/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def show_main(request):
+    return HttpResponse("Hello world")
