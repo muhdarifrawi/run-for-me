a2607db41967ecf1bec004cdd14d7e9804908149
Ubuntu <ubuntu@ip-172-31-92-12.ec2.internal>
2019-11-27 13:22:10 +0000

    edit form is working.

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import request_run, relief_run, request_info, delete_order
+from .views import request_run, relief_run, request_info, delete_order, edit_order
 from django.conf.urls.static import static 
 from django.conf import settings 
 
@@ -8,7 +8,8 @@ urlpatterns = [
     path('relief-run', relief_run, name='relief-run'),
     path('request-info', request_info, name='request-info'),
     path('payment/',include('payment.urls')),
-    path('<sku>/delete-order/',delete_order, name='delete-order')
+    path('<sku>/delete-order/',delete_order, name='delete-order'),
+    path('<sku>/edit-order/',edit_order, name='edit-order')
    
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse, redirect, reverse
 from django.contrib import messages
 from payment.models import orders
+from payment.forms import OrderForm
 from django.conf import settings
 import stripe 
 
@@ -8,7 +9,6 @@ import stripe
 # this is the main views. do not put any payment stuff here.
 
 def request_run(request):
-    # all_orders = orders.objects.all();
     personal_orders = orders.objects.filter(requester=request.user);
     return render(request, "request-run.template.html",{
         'personal_orders':personal_orders
@@ -36,6 +36,27 @@ def delete_order(request, sku):
         
     else:
         orders.objects.filter(sku=sku).delete()
+        personal_orders = orders.objects.filter(requester=request.user);
+        return render(request, "request-run.template.html",{
+            'personal_orders':personal_orders
+        })
+        
+        
+def edit_order(request, sku):
+    if request.method == 'GET':
+        current_order = sku
+        order = orders.objects.get(sku=sku)
+        form = OrderForm(instance=order)
+        # form['full_name'].value()
+        return render(request, "edit.template.html", {
+            'current_order':current_order,
+            'form':form
+        })
+        
+    else:
+        order = orders.objects.get(sku=sku)
+        form = OrderForm(request.POST,instance=order)
+        form.save()
         personal_orders = orders.objects.filter(requester=request.user);
         return render(request, "request-run.template.html",{
             'personal_orders':personal_orders
